08684a2518a01148b6f76f552d5100d0e3bd9b94
fadhil <88018547+vadhil@users.noreply.github.com>
2023-04-24 15:47:26 +0800

    before cleaning

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ function App() {
     <h3 className='text-center trajia m-5'>you are in the room chat</h3>
     < Chat room={room} />
     <button onClick={()=> handleLogOut()} className='btn btn-outline-danger mt-5 mb-5 mx-auto ms-auto text-center' >log out</button>
+    <a className='btn btn-outline-danger text-center mt-5 mb-5 d-block col-3 mx-auto' onClick={()=> handleLogOut()}  href="">sign out</a>
   </div>
   } else {
   return <div>
--- src/pages/login.js
+++ src/pages/login.js
@@ -18,9 +18,16 @@ export const Login = (props) => {
        console.log(result);
 
     }
-    return <div>
-        <h3>sign in with google</h3>
-        <button onClick={()=> signIn()}>sign in</button>
+    return<div>
+    <h2 className='mt-5 alien'> Chat App by Fadhil</h2>
+    <div  className='container bg-light mt-5 py-5 col-8 col-md-4 rounded border shadow '>
+        <div class="row justify-content-center align-items-center g-2">
+            <h3>sign in with google</h3>
+            <button className='btn btn-success col-3 alien shadow-sm' onClick={()=> signIn()}>sign in</button>
+        </div>
+    </div>
+      
+        
    
     </div>
 }
